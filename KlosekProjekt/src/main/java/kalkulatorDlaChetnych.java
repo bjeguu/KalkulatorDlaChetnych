dc8730a1438db88c770db8ae8d30ed86978db174
Barte <bjegub@gmail.com>
2025-05-03 22:06:49 +0200

    Zmiany z komunikatami oraz inne dodatki

--- KlosekProjekt/src/main/java/kalkulatorDlaChetnych.java
+++ KlosekProjekt/src/main/java/kalkulatorDlaChetnych.java
@@ -10,9 +10,10 @@ public class kalkulatorDlaChetnych {
         do {
             System.out.println("\nNo to co dzisiaj liczymy?");
             System.out.println("1. Sprężynka - wahadło");
-            System.out.println("2. Matematyczne machanie");
-            System.out.println("3. Oba na raz!");
-            System.out.println("0. Koniec zabawy");
+            System.out.println("2. Wachadlo matematyczne");
+            System.out.println("3. Oba na raz");
+            System.out.println("4. Pozdrowienia dla pana Kłoska");
+            System.out.println("0. Koniec programu");
             System.out.print("Wybieraj: ");
             wybor = czytnik.nextInt();
             czytnik.nextLine();
@@ -28,11 +29,13 @@ public class kalkulatorDlaChetnych {
                     obliczWahadloSprezynowe(czytnik);
                     obliczWahadloMatematyczne(czytnik);
                     break;
+                case 4:
+                    System.out.println("POZDRAWIIAM PANA KLOSKA!!!!!!!!!!!!!!");
                 case 0:
                     System.out.println("Dzięki za liczenie!");
                     break;
                 default:
-                    System.out.println("Ups, nie rozumiem. Jeszcze raz?");
+                    System.out.println("Zla wartosc");
             }
         } while (wybor != 0);
 
@@ -40,10 +43,10 @@ public class kalkulatorDlaChetnych {
     }
 
     public static void obliczWahadloSprezynowe(Scanner skaner) {
-        System.out.println("\n--- Liczymy sprężynkę ---");
+        System.out.println("\n--- Wachadlo prezynowe");
         System.out.print("Podaj masę (m) w kilogramach: ");
         double masaSprezyny = skaner.nextDouble();
-        System.out.print("A teraz stała sprężystości (k) w niutonach na metr: ");
+        System.out.print("Stala sprężystości (k) w niutonach na metr: ");
         double stalaSprezystosci = skaner.nextDouble();
 
         if (masaSprezyny > 0 && stalaSprezystosci > 0) {
@@ -52,24 +55,24 @@ public class kalkulatorDlaChetnych {
             System.out.println("Okres tego wahadła: " + String.format("%.4f", okresSprezyny) + " sekund");
             System.out.println("Częstotliwość: " + String.format("%.4f", czestotliwoscSprezyny) + " herców");
         } else {
-            System.out.println("Coś tu nie gra z danymi sprężynki (masa i stała muszą być większe od zera!).");
+            System.out.println("Zle dane (masa i stała muszą być większe od zera!).");
         }
     }
 
     public static void obliczWahadloMatematyczne(Scanner input) {
-        System.out.println("\n--- Czas na matematyczne machanie --");
+        System.out.println("\n--- Wachadlo matematyczne --");
         System.out.print("Podaj długość (l) w metrach: ");
         double dlugoscWahadla = input.nextDouble();
-        System.out.print("No i przyspieszenie ziemskie (g) w m/s^2 (dla Ziemi to około 9.81): ");
+        System.out.print("Przyspieszenie (g) w m/s^2 (dla Ziemi to około 9.81) LICZBA MA BYC Z PRZECINKIEM: ");
         double grawitacja = input.nextDouble();
 
         if (dlugoscWahadla > 0 && grawitacja > 0) {
             double okresWahadla = 2 * Math.PI * Math.sqrt(dlugoscWahadla / grawitacja);
             double czestotliwoscWahadla = 1 / okresWahadla;
-            System.out.println("Okres matematycznego machania: " + String.format("%.4f", okresWahadla) + " s");
-            System.out.println("Częstotliwość tego machania: " + String.format("%.4f", czestotliwoscWahadla) + " Hz");
+            System.out.println("Okres matematycznego wachadla: " + String.format("%.4f", okresWahadla) + " s");
+            System.out.println("Częstotliwość tego wachadla: " + String.format("%.4f", czestotliwoscWahadla) + " Hz");
         } else {
-            System.out.println("Oj, chyba coś nie tak z danymi matematycznego wahadła (długość i grawitacja muszą być większe od zera!).");
+            System.out.println("Zle dane (długość i grawitacja muszą być większe od zera!).");
         }
     }
 }
